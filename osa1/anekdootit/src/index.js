a5640b3442a7011552bf607b6e47a9f0937e3d1a
Nora Rytkölä <nora.rytkola@hotmail.fi>
2019-05-22 20:58:15 +0300

    step 1.14 valmis

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -21,27 +21,27 @@ const Tilasto =(aanet, Array )=>{
 
 
 
-const App = (props) => {
-    const listanpituus=props.anecdotes.length;
+const App = (props) => { 
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+
+     ]
+    const listanpituus=anecdotes.length;
     const [selected, setSelected] = React.useState(0);
     let random=(Math.round(5 * Math.random()));
     const [aanet, aanesta]=React.useState(Array(listanpituus).fill(0))
-
+    const kopioi=[...aanet]
     const aanestys =(selected)=> {
-      const kopioi=[...aanet]
       kopioi[selected]+=1
       aanesta(kopioi)
     }
-    const anecdotes = [
-      
-      'If it hurts, do it more often',
-      'Adding manpower to a late software project makes it later!',
-      'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      'Premature optimization is the root of all evil.',
-      'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-
-      ]
+   
 
   
     return (
@@ -49,6 +49,7 @@ const App = (props) => {
         <Lause anecdotes={anecdotes} selected={selected}/>
         <button onClick={() => setSelected(random)}>Next anecdote</button>
         <button onClick={()=> aanestys(selected) }>Vote</button>
+        Ääniä:{aanet[selected]}
         
         
     
