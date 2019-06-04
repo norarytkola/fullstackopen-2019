7c809411228ba6e32c46d2328124bbfd69e03f76
Nora Rytkölä <nora.rytkola@hotmail.fi>
2019-06-04 17:18:22 +0300

    jatkettu maiden nimet

--- osa2/maidennimet/src/App.js
+++ osa2/maidennimet/src/App.js
@@ -3,20 +3,39 @@ import './App.css';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 
-const Maa =(props)=> {
+const Maa =( props)=> {
+  const filtteroi=props.lista
+  if (filtteroi.length < 11 && filtteroi.length >1){
   return(
     <div>
     Country:{props.name}<br/>
-    Capital:{props.capital}
     </div>
-  )
+  )} else if (filtteroi.length===0){
+    return (
+      <div>Ei yhtään tuloksia</div>
+    )
+  } else if (filtteroi.length===1){
+    return (
+      <div>
+        Country: {props.name}<br/>
+        Capital:Capital:{props.capital}
+      </div>
+    )
+  } else if (filtteroi.length >10  && filtteroi.length<250) {
+    return null(
+      <div>Liikaa tuloksia. Tarkenna hakuehtoja. </div>
+    )
+  } else {
+    return null
+  }
 }
+
 
 
 const App=()=> {
 
 const [maat, lisaaMaa]=useState([])
-const [nayta, lisaaNayta]=useState([])
+const [hakuehto, asetaHakuehto]=useState('')
 
 
 useEffect(() => {
@@ -28,26 +47,19 @@ useEffect(() => {
 }, [])
 
 
-
 const hae=(event)=>{
- const haku=event.target.value
- var re = new RegExp(haku, "ig")
- console.log(re)
-    for (let i=0; i<maat.length; i++){
-        if (maat[i].name.search(re)> -1){
-          const lisays = {
-             name:maat[i].name,
-             capital:maat[i].capital}
-             lisaaNayta(nayta.concat(lisays))
-          }}
-        for (let i=0; i<nayta.length; i++){
-          if (nayta[i].name.search(re)< -1){
-            nayta.splice(nayta[i])
-            }}}
+  const haku=event.target.value
+  asetaHakuehto(haku)
+  console.log(haku)
+          }
+const filtteroi=maat.filter(maa=>maa.name.includes(hakuehto))
+console.log(filtteroi)
+      
 
+  
 
-  const rows=() => nayta.map(maa=>
-    <Maa key={maa.name} name={maa.name} capital={maa.capital}
+  const rows=() => filtteroi.map(maa=>
+    <Maa lista={filtteroi} key={maa.id} name={maa.name} capital={maa.capital}
     />)
 
 
