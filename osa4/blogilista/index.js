f119426f07b0d07d8b5044cc0073e2027e120311
Nora Rytkölä <nora.rytkola@hotmail.fi>
2019-08-02 09:59:30 +0300

    blogSchema erotettu

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,27 +1,20 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blogi=require('./models/blogi')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0-9jztp.mongodb.net/blogit?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/api/blogs', (request, response) => {
-  Blog
+  Blogi
     .find({})
     .then(blogs => {
       response.json(blogs)
@@ -29,7 +22,7 @@ app.get('/api/blogs', (request, response) => {
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blogi(request.body)
 
   blog
     .save()
